48a1db8a1bf2a34faeefb50ec6ffcdc7a76daab1
IBIKORANEZA Dieudonne <dieudonne@kali.rca.ac.rw>
2025-01-07 10:58:53 +0200

    Initial Commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintConfigStandard from "eslint-config-standard";
+import pluginTailwindcss from "eslint-plugin-tailwindcss";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      eslintConfigStandard,
+      pluginTailwindcss.recommended,
+      eslintConfigPrettier,
+    ],
+  },
 ];
-
-export default eslintConfig;
