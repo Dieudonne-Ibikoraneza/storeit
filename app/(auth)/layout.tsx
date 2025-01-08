d60a24db73343c201574f2b4ad4b639560b327a0
IBIKORANEZA Dieudonne <dieudonneibikoraneza13@gmail.com>
2025-01-08 13:21:33 +0200

    Finished the reusable auth form

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen">
       <section className="bg-brand p-10 hidden w-1/2 items-center justify-center lg:flex xl:w-2/5">
-        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12 ">
+        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
           <Image
             src="/assets/icons/logo-full.svg"
             alt="Logo"
@@ -24,8 +24,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           <Image
             src="/assets/images/files.png"
             alt="Files"
-            width={342}
-            height={342}
+            width={240}
+            height={240}
             className="transition-all hover:rotate-2 hover:scale-105"
           />
         </div>
@@ -34,7 +34,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mb-16 lg:hidden">
           <Image
-            src="/assets/icon/logo-full-brand.svg"
+            src="/assets/icons/logo-full-brand.svg"
             alt="logo"
             width={224}
             height={82}
